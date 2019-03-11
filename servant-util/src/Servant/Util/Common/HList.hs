688efeb516e53b09d6ec420631a07d29bede4824
martoon <martoon.hsk@gmail.com>
2019-03-11 10:27:19 +0300

    Add `HList` creation from tuple

--- servant-util/src/Servant/Util/Common/HList.hs
+++ servant-util/src/Servant/Util/Common/HList.hs
@@ -2,6 +2,8 @@
 
 module Servant.Util.Common.HList where
 
+-- TODO: move to vinyl one day.
+
 -- | Servant package defines their own 'HList', so we also can (to avoid a large dependency).
 data HList (f :: k -> *) (l :: [k]) where
     HNil :: HList f '[]
@@ -12,3 +14,48 @@ infixr 3 `HCons`
          f a -> HList f as -> HList f (a : as)
 (.*.) = HCons
 infixr 3 .*.
+
+
+class HListFromTuple a where
+    type HListTuple a :: *
+    htuple :: HListTuple a -> a
+
+instance HListFromTuple (HList f '[]) where
+    type HListTuple (HList f '[]) = ()
+    htuple () = HNil
+
+instance HListFromTuple (HList f '[a]) where
+    type HListTuple (HList f '[a]) = f a
+    htuple a = a .*. HNil
+
+instance HListFromTuple (HList f [a, b]) where
+    type HListTuple (HList f [a, b]) = (f a, f b)
+    htuple (a, b) = a .*. b .*. HNil
+
+instance HListFromTuple (HList f [a, b, c]) where
+    type HListTuple (HList f [a, b, c]) = (f a, f b, f c)
+    htuple (a, b, c) = a .*. b .*. c .*. HNil
+
+instance HListFromTuple (HList f [a, b, c, d]) where
+    type HListTuple (HList f [a, b, c, d]) = (f a, f b, f c, f d)
+    htuple (a, b, c, d) = a .*. b .*. c .*. d .*. HNil
+
+instance HListFromTuple (HList f [a, b, c, d, e]) where
+    type HListTuple (HList f [a, b, c, d, e]) = (f a, f b, f c, f d, f e)
+    htuple (a, b, c, d, e) = a .*. b .*. c .*. d .*. e .*. HNil
+
+instance HListFromTuple (HList f [a, b, c, d, e, g]) where
+    type HListTuple (HList f [a, b, c, d, e, g]) = (f a, f b, f c, f d, f e, f g)
+    htuple (a, b, c, d, e, g) = a .*. b .*. c .*. d .*. e .*. g .*. HNil
+
+instance HListFromTuple (HList f [a, b, c, d, e, g, h]) where
+    type HListTuple (HList f [a, b, c, d, e, g, h]) =
+        (f a, f b, f c, f d, f e, f g, f h)
+    htuple (a, b, c, d, e, g, h) =
+        a .*. b .*. c .*. d .*. e .*. g .*. h .*. HNil
+
+instance HListFromTuple (HList f [a, b, c, d, e, g, h, i]) where
+    type HListTuple (HList f [a, b, c, d, e, g, h, i]) =
+        (f a, f b, f c, f d, f e, f g, f h, f i)
+    htuple (a, b, c, d, e, g, h, i) =
+        a .*. b .*. c .*. d .*. e .*. g .*. h .*. i .*. HNil
