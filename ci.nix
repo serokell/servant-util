c7f065808806ae04343ca77851ed49ece5760dc8
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2020-05-05 00:08:19 +0300

    [INT-157] Fix CI (#8)
    * [INT-157] Add `hspec-discover` to build dependencies for tests
    
    * [INT-157] Remove servant-generic from servant-util-beam-pg dependencies
    
    servant-generic has been superseeded by servant and is not in lts-14
    
    * [INT-157] Switch CI to haskell.nix and buildkite

---
+++ ci.nix
@@ -0,0 +1,58 @@
+rec {
+  sources = import ./nix/sources.nix;
+  haskellNix = import sources."haskell.nix" {};
+  pkgs = import sources.nixpkgs haskellNix.nixpkgsArgs;
+
+  local-packages = [
+    { name = "servant-util";         subdirectory = "servant-util"; }
+    { name = "servant-util-beam-pg"; subdirectory = "servant-util-beam-pg"; }
+  ];
+
+  # names of all local packages
+  local-packages-names = map (p: p.name) local-packages;
+
+  # a set mapping package name to package subdirectory
+  subdirectories = pkgs.lib.listToAttrs (map ({ name, subdirectory }:
+    { inherit name; value = subdirectory; }
+  ) local-packages);
+
+  # source with gitignored files filtered out
+  projectSrc = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "servant-util";
+    src = ./.;
+  };
+
+  hs-pkgs = pkgs.haskell-nix.stackProject {
+    src = projectSrc;
+
+    modules = [{
+      # configuration for local packages
+      packages = pkgs.lib.genAttrs local-packages-names (packageName: {
+        # disable optimizations, error on warning
+        package.ghcOptions = "-O0 -Werror";
+
+        # run haddock for local packages
+        doHaddock = true;
+
+        # haskell.nix fails when package.yaml includes a file from the parent
+        # directory, override package src to work around it
+        # https://github.com/input-output-hk/haskell.nix/issues/485
+        src = projectSrc;
+        postUnpack = "sourceRoot=$sourceRoot/${subdirectories.${packageName}}";
+      });
+
+      # don't haddock dependencies
+      doHaddock = false;
+    }];
+  };
+
+  # component set for each local package
+  packages = pkgs.lib.genAttrs local-packages-names (pkg: hs-pkgs.${pkg}.components);
+
+  # returns a list of all components for a package
+  get-package-components = pkg: with pkg.components; with pkgs.lib;
+    optional (pkg ? library) library ++ attrValues exes ++ attrValues tests;
+
+  # a list of all components from all local packages
+  all-components = pkgs.lib.concatMap (pkg: get-package-components hs-pkgs.${pkg}) local-packages-names;
+}
